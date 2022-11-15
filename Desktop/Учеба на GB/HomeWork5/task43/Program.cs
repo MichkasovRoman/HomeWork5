a48bd41825d54fef18f0256c04d2a03937f67140
Michkasov Roman <mihjob@list.ru>
2022-11-15 21:35:15 +0300

    task43

---
+++ Desktop/Учеба на GB/HomeWork5/task43/Program.cs
@@ -0,0 +1,13 @@
+﻿// задача 43. Напишите программу, которая найдет точку пересечения двух прямых,
+//заданных уравнениями y = k1*x + b1, y = k2*x + b2.
+
+int[] EquationLine1 ()
+{
+    int[] equation = new int[2];
+    for (int i = 0; i <= 1; i++)
+    {
+        Console.Write($"Введите угловой коэффийиент первой прямой: ");
+        ResultArray[i] = int.Parse(Console.ReadLine()!);
+    }
+    return ResultArray;
+}
